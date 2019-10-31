1962bbdc5cdaa0fa54241be3ffafacecb47681c1
dschoenicke <dschoenicke@gmx.de>
2019-10-31 12:52:02 +0100

    Setup project

---
+++ src/main/java/uml_unittest_generator/App.java
@@ -0,0 +1,13 @@
+package uml_unittest_generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
