061d654c8ed63e80ef342001050142050d7bd6b7
dschoenicke <dschoenicke@gmx.de>
2019-10-31 14:17:26 +0100

    #5 Setup tests
    Setup basic test suite for jaxb representation

--- input/md-jaxb/src/test/java/md_jaxb/JaxbRepresentationTests.java
+++ input/md-jaxb/src/test/java/md_jaxb/JaxbRepresentationTests.java
@@ -1,38 +1,27 @@
-package com.dschoenicke.md_jaxb;
+package md_jaxb;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JaxbRepresentationTests
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	private JaxbRepresentation representation;
+	
+	@Before
+	public void init() throws JAXBException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("md_jaxb.xml").getFile());
+		representation = new JaxbRepresentation(file.getAbsolutePath());
+	}
+	
+	@Test
+	public void modelSizeTest() {
+		assertEquals(representation.getXmi().getModel().getPackagedElements().size(), 7);
+	}
 }
