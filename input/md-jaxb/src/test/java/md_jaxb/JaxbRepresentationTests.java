f139bb88147ae095c88968f5bdfe6b1c81240116
dschoenicke <dschoenicke@gmx.de>
2019-11-01 15:41:20 +0100

    #5 PackagedElement Test Cases
    Add test cases which handle the PackagedElement class as a vicarious example for the whole model

--- input/md-jaxb/src/test/java/md_jaxb/JaxbRepresentationTests.java
+++ input/md-jaxb/src/test/java/md_jaxb/JaxbRepresentationTests.java
@@ -1,6 +1,7 @@
 package md_jaxb;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 
@@ -9,37 +10,72 @@ import javax.xml.bind.JAXBException;
 import org.junit.Before;
 import org.junit.Test;
 
-import model.OwnedAttribute;
 import model.PackagedElement;
 
 public class JaxbRepresentationTests
 {
 	private JaxbRepresentation representation;
+	private PackagedElement element;
+	
+	public PackagedElement checkForPackagedElement(PackagedElement elementToCheck, String name) {
+		if (elementToCheck.getName().equals(name)) {
+			return elementToCheck;
+		}
+		
+		if (elementToCheck.getPackagedElements().isEmpty()) {
+			return null;
+		}
+		
+		PackagedElement returnElement = null;
+		
+		for (PackagedElement childElement : elementToCheck.getPackagedElements()) {
+			returnElement = checkForPackagedElement(childElement, name);
+			
+			if (returnElement != null) {
+				break;
+			}
+		}
+		
+		return returnElement;
+	}
 	
 	@Before
 	public void init() throws JAXBException {
 		ClassLoader classLoader = getClass().getClassLoader();
 		File file = new File(classLoader.getResource("md_jaxb.xml").getFile());
 		representation = new JaxbRepresentation(file.getAbsolutePath());
+		
+		for (PackagedElement packagedElement : representation.getXmi().getModel().getPackagedElements()) {
+			element = checkForPackagedElement(packagedElement, "PackagedElement");
+			
+			if (element != null) {
+				break;
+			}
+		}
 	}
 	
 	@Test
-	public void modelSizeTest() {
-		assertEquals(representation.getXmi().getModel().getPackagedElements().size(), 32);
-		
-		for (PackagedElement element : representation.getXmi().getModel().getPackagedElements()) {
-			for (OwnedAttribute attribute : element.getOwnedAttributes()) {
-				System.out.println(element.getName() + "." + attribute.getName());
-				
-				if (attribute.getDataType() != null) {
-					System.out.println(attribute.getDataType().getHref());
-				}
-				else {
-					System.out.println(attribute.getAssociationType());
-				}
-			}
- 		}
+	public void packageElementFoundTest() {
+		assertNotNull(element);
 	}
 	
+	@Test 
+	public void attributeTest() {
+		assertEquals(element.getId(), "_19_0_1_62d0212_1572527221607_338771_4722");
+		assertEquals(element.getName(), "PackagedElement");
+		assertEquals(element.getType(), "uml:Class");
+		assertEquals(element.getIsAbstract(), null);
+		assertEquals(element.getIsFinal(), null);
+		assertEquals(element.getIsStatic(), null);
+	}
 	
+	@Test
+	public void ownedAttributeTest() {
+		assertEquals(element.getOwnedAttributes().size(), 22);
+	}
+	
+	@Test
+	public void ownedOperationsTest() {
+		assertEquals(element.getOwnedOperations().size(), 41);
+	}
 }
