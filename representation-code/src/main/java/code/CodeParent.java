c837628f8bf95edca2bd78f352f9a8a9c66bd1d9
dschoenicke <dschoenicke@gmx.de>
2019-11-25 23:13:19 +0100

    #18 Extend CodeParent
    Add name getter to the interface

--- representation-code/src/main/java/code/CodeParent.java
+++ representation-code/src/main/java/code/CodeParent.java
@@ -1,11 +1,11 @@
 package code;
 
 /**
- * Interface implemented by the classes of the model to reference a parent class
+ * Interface implemented by the classes of the model to reference a parent class, provides a method the get its name
  * 
  * @author dschoenicke
  *
  */
 public interface CodeParent {
-	
+	public String getName();
 }
