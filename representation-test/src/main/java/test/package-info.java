181632033f1a0ed0e2db377581e168c2b5262a81
dschoenicke <dschoenicke@gmx.de>
2019-12-01 14:28:48 +0100

    #27 Add diagram and description of test-representation

---
+++ representation-test/src/main/java/test/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains all model classes of the test representation.<br>
+ * 
+ * <img src="../../diagrams/test.svg" alt="class-diagram">
+ * 
+ * @author dschoenicke
+ * 
+ */
+package test;
