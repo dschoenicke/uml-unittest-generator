35d92a55f41ee7d5da22982a8c44c19d077005a8
dschoenicke <dschoenicke@gmx.de>
2019-12-02 22:32:22 +0100

    #31 Remove superfluous exception test

--- converter-code-test/src/test/java/codetestconverter/packages/CodePackageTest.java
+++ converter-code-test/src/test/java/codetestconverter/packages/CodePackageTest.java
@@ -6,10 +6,8 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 
-import code.CodeClass;
 import code.CodePackage;
 import code.CodeRepresentation;
-import code.CodeVisibility;
 import codetestconverter.temporary.TemporaryModel;
 import test.TestPackage;
 import test.TestRepresentation;
@@ -57,10 +55,9 @@ public class CodePackageTest {
 	
 	/**
 	 * Tests the {@link PackageConverter#convertPackages} method
-	 * @throws Exception exception if the {@link test.TestParent} is neither of type {@link test.TestRepresentation}, nor {@link test.TestPackage}.
 	 */
 	@Test
-	public void testPackageConversion() throws Exception {
+	public void testPackageConversion() {
 		PackageConverter.convertPackages(mockCodeRepresentation, mockTestRepresentation, new TemporaryModel());
 		TestPackage testPackage = mockTestRepresentation.getPackages().get(0);
 		assertNotNull(testPackage);
@@ -68,14 +65,5 @@ public class CodePackageTest {
 		testPackage = testPackage.getPackages().get(0);
 		assertNotNull(testPackage);
 		assertEquals(testPackage.getName(), mockSubPackage.getName());
-	}
-	
-	/**
-	 * Tests whether an exception is thrown, if an invalid {@link code.CodeParent} is passed.
-	 * @throws Exception exception if the {@link code.CodeParent} is neither of type {@link code.CodeRepresentation}, nor {@link code.CodePackage}.
-	 */
-	@Test(expected=Exception.class)
-	public void testPackageConversionException() throws Exception {
-		PackageConverter.convertPackages(new CodeClass("", mockPackage, CodeVisibility.DEFAULT, false, false, false), mockTestRepresentation, new TemporaryModel());
 	}
 }
