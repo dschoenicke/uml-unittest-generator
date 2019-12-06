55204d43a3f2cbe32e889500cf414947e85916d2
dschoenicke <dschoenicke@gmx.de>
2019-12-06 13:40:29 +0100

    #32 Setup output-junit-converter

---
+++ output-junit-converter/src/main/java/outputjunit/OutputJUnitConverter.java
@@ -0,0 +1,12 @@
+package outputjunit;
+
+import test.TestRepresentation;
+
+public class OutputJUnitConverter 
+{
+	public OutputJUnitConverter() {}
+	
+	public void convertToJUnitTestFiles(TestRepresentation testRepresentation, String outputDirectory) {
+		
+	}
+}
---
+++ output-junit-converter/src/test/java/outputjunit/OutputJunitConverterTest.java
@@ -0,0 +1,5 @@
+package outputjunit;
+
+public class OutputJunitConverterTest {
+
+}
