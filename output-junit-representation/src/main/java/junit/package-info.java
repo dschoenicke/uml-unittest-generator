d2baac3991183c985ac2e0709570d4be87ba5fdc
dschoenicke <dschoenicke@gmx.de>
2020-01-24 13:55:27 +0100

    #16 Add junit representation diagram and package-info

---
+++ output-junit-representation/src/main/java/junit/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains all model classes of the junit representation, used to fill the mustache template for test generation.<br>
+ * 
+ * <img src="../../diagrams/junit.svg" alt="class-diagram">
+ * 
+ * @author dschoenicke
+ */
+package junit;
